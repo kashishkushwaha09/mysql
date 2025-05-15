70a38e35f793998213148dafb25ee7480dabdf91
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-15 16:30:54 +0530

    tables created

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connection=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'Khush@123',
-    database:'testdb'
+    database:'busbookingapp'
 })
 connection.connect((err)=>{
     if(err){
@@ -14,17 +14,57 @@ connection.connect((err)=>{
         return;
     }
     console.log("connection has been created");
-    const creationQuery=`create table Students(
+    const users=`create table Users(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    email VARCHAR(20))`
-    connection.execute(creationQuery,(err)=>{
+    name VARCHAR(255),
+    email VARCHAR(255)
+    )`
+    const buses=`create table Buses(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    busNumber VARCHAR(255),
+    totalSeats INT,
+    availableSeats INT
+    )`
+    const bookings=`create table Bookings(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    seatNumber INT
+    )`
+    const payments=`create table Payments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amountPaid DECIMAL(10,2),
+    paymentStatus VARCHAR(255)
+    )`
+    connection.execute(users,(err)=>{
         if(err){
             console.log(err);   
             connection.end();
             return;
         }
-        console.log('table has been created');
+        console.log('users has been created');
+    })
+    connection.execute(buses,(err)=>{
+        if(err){
+            console.log(err);   
+            connection.end();
+            return;
+        }
+        console.log('buses has been created');
+    })
+    connection.execute(bookings,(err)=>{
+        if(err){
+            console.log(err);   
+            connection.end();
+            return;
+        }
+        console.log('bookings has been created');
+    })
+    connection.execute(payments,(err)=>{
+        if(err){
+            console.log(err);   
+            connection.end();
+            return;
+        }
+        console.log('payments has been created');
     })
 })
 
