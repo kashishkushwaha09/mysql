2f2899c954964cf02ec774da590de9370a9eccd8
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-16 14:40:02 +0530

    users crud operation using sequelize

--- controller/userController.js
+++ controller/userController.js
@@ -1,81 +1,71 @@
 const db=require('../utils/db-connection');
+const User=require('../models/userModel')
 // Retrieve all users from the database.
-const retrieveEntries=(req,res)=>{
-    const readQuery=`SELECT * FROM users`;
-    db.execute(readQuery,(err,results)=>{
-        if(err){
-        console.log(err.message);
-        res.status(500).send(err.message);
-        db.end();
-        return;
+const retrieveEntries=async (req,res)=>{
+ try {
+    const user=await User.findAll();
+    if(!user){
+        res.status(404).send("student not found"); 
     }
-     res.status(200).send(results);
-    })
+     res.status(200).send(user);
+ } catch (error) {
+     console.log(error);
+         res.status(500).send("Server error:-unable to retrieve data");
+ }
 }
 // Add a new user.
-const addEntries=(req,res)=>{
-const {email,name}=req.body;
-const InsertQuery=`INSERT INTO users (name,email) VALUES (?,?)`;
-db.execute(InsertQuery,[name,email],(err)=>{
-    if(err){
-        console.log(err.message);
-        res.status(500).send(err.message);
-        db.end();
-        return;
+const addEntries=async (req,res)=>{
+try {
+    const {name,email}=req.body;
+     const user=await User.create({
+        name,email
+     }) 
+     res.status(201).send(`User with name ${name} successfully created`);
+    } catch (error) {
+        console.log(error);
+         res.status(500).send("Server error:-unable to make an entry");
+        
     }
-    console.log("value has been inserted");
-    res.status(200).send(`User with name ${name} successfully added`);
-})
 }
-const updateEntries=(req,res)=>{
+const updateEntries=async (req,res)=>{
+    try {
     const {id}=req.params;
     const {name,email}=req.body;
-    let updateQuery,fields;
-    if(!name && !email){
-     res.status(400).send('provide essentials to update');
-     db.end(); 
-     return;
-    }else if(!name && email){
-    updateQuery=`UPDATE users set email=? WHERE id=?`;
-     fields=[email,id];
-    }else if(name && !email){
-    updateQuery=`UPDATE users set name=? WHERE id=?`;
-     fields=[name,id];
-    }else{
-    updateQuery=`UPDATE users set name=?,email=? WHERE id=?`;
-     fields=[name,email,id];
+    const user=await User.findByPk(id);
+    if(!user){
+        res.status(404).send("user not found");
     }
-    db.execute(updateQuery,fields,(err,result)=>{
-        if(err){
-            console.log(err.message);
+    if(name){
+    user.name=name;
+    }
+    if(email){
+        user.email=email;
+    }
+    
+    await user.save();
+    res.status(200).send("user has been updated");
+ } catch (error) {
+             console.log(err);
         res.status(500).send(err.message);
-        db.end();
-        return;
-        }
-        if(result.affectedRows===0){
-            res.status(404).send("User not found");
-            return;
-        }
-        res.status(200).send("user has been updated");
-    })
+ }
 }
-const deleteEntry=(req,res)=>{
-    const {id}=req.params;
-    const deleteQuery=`DELETE FROM users WHERE id=?`;
-    db.execute(deleteQuery,[id],(err,results)=>{
-    if(err){
-        console.log(err.message);
+const deleteEntry=async (req,res)=>{
+     try {
+        const {id}=req.params; 
+        const user=await User.destroy({
+            where:{
+                id:id
+            }
+        })
+        if(!user){
+            res.status(404).send('User not found');
+        }
+        res.status(200).send(`user with id ${id} is deleted`);
+    } catch (err) {
+            console.log(err);
         res.status(500).send(err.message);
-        db.end();
-        return;
     }
-    console.log("user has been deleted");
-    if(results.affectedRows===0){
-        res.status(404).send("User not found");
-        return;
-    }
-    res.status(200).send(`User with id ${id} is deleted`);
-    })
+
 }
 
 module.exports={retrieveEntries,addEntries,updateEntries,deleteEntry};
--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app=express();
 const db=require('./utils/db-connection');
 const userRouter=require('./routes/usersRoutes');
 const busRouter=require('./routes/busesRoutes');
+const userModel=require('./models/userModel');
 app.use(express.json());
 app.use('/api/users',userRouter);
 app.use('/api/buses',busRouter);
+
+db.sync({alter:true}).then(()=>{
 app.listen(3000,()=>{
     console.log("server is listening on port 3000");
+})
+})
+.catch((err)=>{
+    console.log(err);
 })
